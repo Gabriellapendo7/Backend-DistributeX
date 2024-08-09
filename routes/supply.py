e9bf6660e0454f44eac2abcd7613fe03fce59dc1
brandondando <dandobrandon0@gmail.com>
2024-08-09 12:15:57 +0300

    Modified Supply.py file

--- routes/supply.py
+++ routes/supply.py
@@ -1,19 +1,18 @@
 from flask import Blueprint, request, jsonify
 from config import db
-from models import Supply  # Ensure this import matches your models file structure
+from models import Supply  e
 
 supply_bp = Blueprint('supply', __name__)
 
 @supply_bp.route('/api/supply', methods=['POST'])
 def add_supply():
-    # Get data from the request
+
     data = request.get_json()
 
-    # Validate input data (You can add more validation as needed)
     if not all(key in data for key in ('AdminID', 'ManufacturerID', 'supply_name', 'quantity_ordered', 'order_date', 'ProductID')):
         return jsonify({'message': 'Missing data'}), 400
 
-    # Create a new Supply instance
+
     new_supply = Supply(
         AdminID=data['AdminID'],
         ManufacturerID=data['ManufacturerID'],
@@ -23,7 +22,7 @@ def add_supply():
         ProductID=data['ProductID']
     )
 
-    # Add the supply to the database
+ 
     db.session.add(new_supply)
     db.session.commit()
 
